0761708a941c2486f96655a5933691847d4c74cc
mardabar <mossman1769@gmail.com>
2025-11-20 20:01:39 -0600

    Finishing touches to autos, disabled other autos we not using

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarBlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarBlueAuto.java
@@ -10,6 +10,7 @@ import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,6 +22,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+@Disabled
 @Autonomous(name = "ADONT USE THIS", group = "autonomous")
 public class FarBlueAuto extends OpMode{
 
